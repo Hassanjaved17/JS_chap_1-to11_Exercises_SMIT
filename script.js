4cc2aaf2ea00d63688dc9f7aa61fbda37dc8fe15
Hassanjaved17 <hassanaptech9@gmail.com>
2025-12-06 00:08:35 +0500

    Complete JS exercises: strings, numbers, and alerts

--- script.js
+++ script.js
@@ -1,4 +1,76 @@
 console.log("js is running");
 
-var uname = "hassan";
-alert(uname);
+// task
+// alert("Hello, World!");
+// task
+// alert("Hassan Javed");
+// task
+// alert(2025);
+// task
+// var message = "Hello I Am Frontend-Developer"
+// alert(message);
+// task
+// var first = "Hello";
+// var second = "World!"
+// var combined = first + second;
+// alert(combined);
+// task
+// var userName = "Hassan Javed";
+// task
+
+// var newMessage = "Nice to meet you!"
+// alert(newMessage);
+// task
+
+// var firstName = "Hassan";
+// var lastName = "Javed";
+
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// task
+
+// let favoriteQuote = "Work hard in silence, let success make the noise.";
+// console.log(favoriteQuote);
+
+// task
+
+// var city = "Karachi";
+// alert("Welcome to " + city + "!");
+// task
+
+// var age = 18;
+// console.log(age);
+
+// task
+// var num1 = 15;
+// var num2 = 13;
+// var sum = num1 + num2;
+// console.log(sum); 
+
+// task
+
+// var num3 = 50;
+// var num4 = 20;
+
+// var difference = num1 - num2;
+// console.log(difference); 
+// task
+
+// var num5 = 5;
+// var num6 = 10;
+
+// var product = num5 * num6;
+// console.log(product);
+
+// task
+
+// let age = 20;
+
+// alert("You are " + age + " years old.");
+// task
+
+// var x = 10;
+// var y = 5;
+// var sum = x + y;
+// console.log(sum);
